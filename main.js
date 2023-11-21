0a852f5c1d1117d587e7c19a373189857a61abc5
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2023-11-21 13:47:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -245,7 +245,7 @@ function update()
     // updating text
     document.getElementById("lives").innerHTML = "Lives: " + liveRemains;
     document.getElementById("filled").innerHTML = "Filled: " + scorePrc + "%/60%";
-    document.getElementById("hardness").innerHTML = "Hardness: " + ;
+    document.getElementById("hardness").innerHTML = "Hardness: " + hardnessMode;
 
 
 
